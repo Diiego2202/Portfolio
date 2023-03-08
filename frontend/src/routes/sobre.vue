c85354580d4ad45df50d57dd5671b67f142ecdd7
Diego Calciolari Gordiano <diego.gordiano@acad.espm.br>
2023-03-08 16:45:40 -0300

    Primeiro commit

---
+++ frontend/src/routes/sobre.vue
@@ -0,0 +1,20 @@
+<template>
+    <NavBar />
+    <h1>Sobre</h1>
+  </template>
+  
+<script>
+    import NavBar from './components/NavBar.vue'
+
+    export default {
+    name: 'Sobre',
+    components: {
+        NavBar
+    },
+    data() {
+        return {
+
+        }
+    }
+    }
+</script>
